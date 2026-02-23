a7b091a1334c0d077b8cb03da9415519c67530df
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-02-23 14:47:44 +0300

    feat (dbContext): add dbContext #3

--- SmartEvent.Backend/SmartEvent.Backend.Persistence/ApplicationDbContext.cs
+++ SmartEvent.Backend/SmartEvent.Backend.Persistence/ApplicationDbContext.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using SmartEvent.Backend.Core.Models;
+using SmartEvent.Backend.Persistence.Configurations;
+
 namespace SmartEvent.Backend.Persistence;
 
-public class ApplicationDbContext
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): DbContext(options)
 {
-    
+    public DbSet<User>  Users { get; set; }
+    public DbSet<Event> Events { get; set; }
+    public DbSet<Attendance>  Attendances { get; set; }
+    public DbSet<QrCode> QrCodes { get; set; }
+    public DbSet<Registration> Registrations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new EventConfiguration());
+        modelBuilder.ApplyConfiguration(new AttendanceConfiguration());
+        modelBuilder.ApplyConfiguration(new EventOrganizerConfiguration());
+        modelBuilder.ApplyConfiguration(new QrCodeConfiguration());
+        modelBuilder.ApplyConfiguration(new RegistrationConfiguration());
+        
+        base.OnModelCreating(modelBuilder);
+    }
 }
