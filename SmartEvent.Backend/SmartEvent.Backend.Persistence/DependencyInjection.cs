de23059ff06f3b59f37bc3e85377fac272e6d367
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-02-24 14:44:29 +0300

    feat (dbContext): add using dbContext to DI #3

--- SmartEvent.Backend/SmartEvent.Backend.Persistence/DependencyInjection.cs
+++ SmartEvent.Backend/SmartEvent.Backend.Persistence/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +8,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });   
+        
         return services;
     }
 }
