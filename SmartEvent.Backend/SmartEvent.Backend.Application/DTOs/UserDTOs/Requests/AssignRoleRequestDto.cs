f2a7fc490598156a08201d50e8650210c5fb2412
Kirill Drach <kirilldrachkirill@gmail.com>
2026-02-24 16:29:11 +0300

    feat(user): add DTOs

---
+++ SmartEvent.Backend/SmartEvent.Backend.Application/DTOs/UserDTOs/Requests/AssignRoleRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using SmartEvent.Backend.Core.Enums;
+
+namespace SmartEvent.Backend.Application.DTOs.UserDTOs.Requests
+{
+    public class AssignRoleRequestDto
+    {
+        public Guid UserId { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
