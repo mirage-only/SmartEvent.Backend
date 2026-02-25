a9034dfca9c47ced33b734112c23e0c57f5943c7
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-02-25 17:54:13 +0300

    feat (jwt): edit user service to JWT way #7

--- SmartEvent.Backend/SmartEvent.Backend.Application/Services/UserService.cs
+++ SmartEvent.Backend/SmartEvent.Backend.Application/Services/UserService.cs
@@ -7,28 +7,60 @@ using SmartEvent.Backend.Core.Models;
 
 namespace SmartEvent.Backend.Application.Services
 {
-    public class UserService(IUserRepository userRepository, IMapper mapper, IPasswordHasher passwordHasher) : IUserService
+    public class UserService(IUserRepository userRepository, IMapper mapper,
+        IPasswordHasher passwordHasher, IJwtService jwtService) : IUserService
     {
-        public Task<AssignRoleResponseDto> AssignRoleAsync(AssignRoleRequestDto request)
+        public async Task<AuthorizeUserResponseDto> RegisterUserAsync(RegisterUserRequestDto? request)
         {
-            throw new NotImplementedException();
-        }
+            if (request == null) throw new ArgumentNullException(nameof(request));
 
+            var existing = await userRepository.GetUserByEmail(request.Email);
+            if (existing != null)
+            {
+                throw new Exception("User with this email already exists");
+            }
+
+            var user = mapper.Map<User>(request);
+            user.PasswordHash = passwordHasher.Hash(request.Password);
+            user.UserRole = Core.Enums.UserRole.Student;
+            user.CreatedAt = DateTime.UtcNow;
+            user.UpdatedAt = DateTime.UtcNow;
+
+            await userRepository.AddUser(user);
+
+            var token = jwtService.GenerateJwtToken(user);
+
+            var response = new AuthorizeUserResponseDto
+            {
+                JwtToken = token
+            };
+            
+            return response;
+        }
+        
         public async Task<AuthorizeUserResponseDto> AuthorizeUserAsync(LoginUserRequestDto? request)
         {
-            string invalidIpnutExeption = "Invalid email or password";
+            string invalidInputExeption = "Invalid email or password";
             if (request == null) throw new ArgumentNullException(nameof(request));
 
             var user = await userRepository.GetUserByEmail(request.Email);
 
             if(user == null) 
-                throw new Exception(invalidIpnutExeption);
-            if(!passwordHasher.Verify(user.PasswordHash, request.Password))
-                throw new Exception(invalidIpnutExeption);
+                throw new Exception(invalidInputExeption);
+            
+            if (passwordHasher.Verify(user.PasswordHash, request.Password))
+            {
+                var token = jwtService.GenerateJwtToken(user);
 
-            var response = mapper.Map<AuthorizeUserResponseDto>(user);
-            response.Token = "jwt will be here";
-            return response;
+                var responseDto = new AuthorizeUserResponseDto()
+                {
+                    JwtToken = token
+                };
+                
+                return responseDto;
+            }
+            
+            throw new Exception(invalidInputExeption);
         }
 
         public Task DeleteUserAsync(Guid id)
@@ -50,29 +82,6 @@ namespace SmartEvent.Backend.Application.Services
         {
             throw new NotImplementedException();
         }
-        
-        public async Task<AuthorizeUserResponseDto> RegisterUserAsync(RegisterUserRequestDto? request)
-        {
-            if (request == null) throw new ArgumentNullException(nameof(request));
-
-            var existing = await userRepository.GetUserByEmail(request.Email);
-            if (existing != null)
-            {
-                throw new Exception("User with this email already exists");
-            }
-
-            var user = mapper.Map<User>(request);
-            user.PasswordHash = passwordHasher.Hash(request.Password);
-            user.UserRole = Core.Enums.UserRole.Student;
-            user.CreatedAt = DateTime.UtcNow;
-            user.UpdatedAt = DateTime.UtcNow;
-
-            await userRepository.AddUser(user);
-
-            var response = mapper.Map<AuthorizeUserResponseDto>(user);
-            response.Token = "jwt will be here";
-            return response;
-        }
 
         public Task<UpdateUserResponseDto> UpdateUserAsync(UpdateUserRequestDto request)
         {
@@ -85,6 +94,11 @@ namespace SmartEvent.Backend.Application.Services
         }
 
         public Task<bool> UserExistsByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public Task<AssignRoleResponseDto> AssignRoleAsync(AssignRoleRequestDto request)
         {
             throw new NotImplementedException();
         }
