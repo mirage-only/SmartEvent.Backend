b83a309ce4cfb7961cd49b0d4a8265c325ed4c9e
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-12 18:31:28 +0300

    feat (controller): add registration for event #4

--- SmartEvent.Backend/SmartEvent.Backend.Api/Controllers/RegistrationController.cs
+++ SmartEvent.Backend/SmartEvent.Backend.Api/Controllers/RegistrationController.cs
@@ -1,6 +1,19 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmartEvent.Backend.Application.Interfaces.IServices;
+
 namespace SmartEvent.Backend.Api.Controllers;
 
-public class RegistrationController
+[ApiController]
+[Route("registration")]
+[Authorize]
+public class RegistrationController(IRegistrationService registrationService): BaseApiController
 {
-    
+    [HttpPost("regForEvent/{eventId:guid}")]
+    public async Task<IActionResult> RegisterForEvent([FromRoute] Guid eventId)
+    {
+        var response = await registrationService.RegisterForEventAsync(eventId);
+        
+        return HandleResult(response);
+    }
 }
