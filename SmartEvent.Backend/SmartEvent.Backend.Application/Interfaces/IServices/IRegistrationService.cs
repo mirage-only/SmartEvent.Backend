1194e28f3ec448e3689a39fbefda7ebc17b3a8e3
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-13 14:57:51 +0300

    feat (RegService): add method for check existing reg #4

--- SmartEvent.Backend/SmartEvent.Backend.Application/Interfaces/IServices/IRegistrationService.cs
+++ SmartEvent.Backend/SmartEvent.Backend.Application/Interfaces/IServices/IRegistrationService.cs
@@ -5,4 +5,6 @@ namespace SmartEvent.Backend.Application.Interfaces.IServices;
 public interface IRegistrationService
 {
     public Task<Result<Guid>> RegisterForEventAsync(Guid eventId);
+    
+    public Task<Result<Guid>> IsRegistrationExistAsync(Guid eventId);
 }
