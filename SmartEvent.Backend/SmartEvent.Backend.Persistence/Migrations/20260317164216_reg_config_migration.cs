884d3c6f18d4fc37718b9d93060994a89081fda4
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-18 10:28:06 +0300

    feat: add new migration #69

---
+++ SmartEvent.Backend/SmartEvent.Backend.Persistence/Migrations/20260317164216_reg_config_migration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartEvent.Backend.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class reg_config_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Registrations_Users_UserId",
+                table: "Registrations");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Registrations_Users_UserId",
+                table: "Registrations",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Registrations_Users_UserId",
+                table: "Registrations");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Registrations_Users_UserId",
+                table: "Registrations",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
