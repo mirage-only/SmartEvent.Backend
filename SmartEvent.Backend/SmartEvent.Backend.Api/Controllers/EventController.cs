3829df85009d22b7b87e97aa5a65db824bdee317
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-18 10:25:10 +0300

    feat (eventController): add adding method #69

--- SmartEvent.Backend/SmartEvent.Backend.Api/Controllers/EventController.cs
+++ SmartEvent.Backend/SmartEvent.Backend.Api/Controllers/EventController.cs
@@ -27,13 +27,13 @@ public class EventController(IEventService eventService): BaseApiController
         return HandleResult(response);
     }
 
-    /*[HttpPut("updateEvent")]
-    public async Task<IActionResult> UpdateEvent([FromBody] EventUpdateDto @event)
+    [HttpPost("add")]
+    public async Task<IActionResult> AddEventAsync(AddEventDto addEventDto)
     {
-        string response = "fe";
-
+        var response = await eventService.AddEventAsync(addEventDto);
+        
         return HandleResult(response);
-    }*/
+    }
     
     [HttpDelete("delete/{eventId:guid}")]
     public async Task<IActionResult> DeleteEvent(Guid eventId)
