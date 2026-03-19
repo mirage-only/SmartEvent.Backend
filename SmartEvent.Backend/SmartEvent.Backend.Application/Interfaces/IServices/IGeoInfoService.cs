4a5b240210e9979ad6d6d633ade3e859441753c3
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-20 01:37:32 +0300

    feat: add method to calculate distence #73

--- SmartEvent.Backend/SmartEvent.Backend.Application/Interfaces/IServices/IGeoInfoService.cs
+++ SmartEvent.Backend/SmartEvent.Backend.Application/Interfaces/IServices/IGeoInfoService.cs
@@ -1,9 +1,11 @@
 using SmartEvent.Backend.Application.DTOs;
 using SmartEvent.Backend.Core.Common;
+using SmartEvent.Backend.Core.Models;
 
 namespace SmartEvent.Backend.Application.Interfaces.IServices;
 
 public interface IGeoInfoService
 {
-    public Task<Result<LocationDto>> GetLocationByAddressAsync(string address);
+    public Task<Result<LocationDto>> GetEventLocationByAddressAsync(string address);
+    public double CalculateDistanceDifference(double eventLat, double eventLong, double userLat, double userLong);
 }
