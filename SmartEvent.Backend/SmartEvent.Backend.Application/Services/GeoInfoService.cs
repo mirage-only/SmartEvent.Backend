b51d5978021489536b636b48c176ec7bc30917b5
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-20 01:37:03 +0300

    refactor: method name #73

--- SmartEvent.Backend/SmartEvent.Backend.Application/Services/GeoInfoService.cs
+++ SmartEvent.Backend/SmartEvent.Backend.Application/Services/GeoInfoService.cs
@@ -2,12 +2,13 @@ using System.Net;
 using SmartEvent.Backend.Application.DTOs;
 using SmartEvent.Backend.Application.Interfaces.IServices;
 using SmartEvent.Backend.Core.Common;
+using SmartEvent.Backend.Core.Models;
 
 namespace SmartEvent.Backend.Application.Services;
 
 public class GeoInfoService(ILocationService locationService): IGeoInfoService
 {
-    public async Task<Result<LocationDto>> GetLocationByAddressAsync(string address)
+    public async Task<Result<LocationDto>> GetEventLocationByAddressAsync(string address)
     {
         if(string.IsNullOrWhiteSpace(address)) 
             return Result<LocationDto>
@@ -19,4 +20,9 @@ public class GeoInfoService(ILocationService locationService): IGeoInfoService
         return location.IsSuccess ? location : Result<LocationDto>
             .Failure("Something went wrong", HttpStatusCode.FailedDependency);
     }
+        
+        return distance;
+    }
+    
+    private double ToRadians(double angle) => angle * (Math.PI / 180);
 }
