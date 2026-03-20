2190b70b83296d3ecabb67136c43230f104b9c41
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-03-20 12:36:12 +0300

    feat (QrController): add and implement #73

--- SmartEvent.Backend/SmartEvent.Backend.Api/Controllers/QrCodeController.cs
+++ SmartEvent.Backend/SmartEvent.Backend.Api/Controllers/QrCodeController.cs
@@ -1,6 +1,19 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmartEvent.Backend.Application.Interfaces.IServices;
+
 namespace SmartEvent.Backend.Api.Controllers;
 
-public class QrCodeController
+[ApiController ]
+[Route("qr")]
+[Authorize]
+public class QrCodeController(IQrService qrService): BaseApiController
 {
-    
+    [HttpGet("getActiveQr/{id:guid}")]
+    public async Task<IActionResult> GetActiveQrCode(Guid? id)
+    {
+        var response = await qrService.GetOrGenerateQrCodeAsync(id);
+        
+        return HandleResult(response);
+    }
 }
