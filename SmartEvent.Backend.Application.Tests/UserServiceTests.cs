0aef6b9ba9812f09b0222ab731aef60648a9fe85
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-07-28 13:56:23 +0300

    feat: add authorize success test #12

---
+++ SmartEvent.Backend.Application.Tests/UserServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using NSubstitute;
+using SmartEvent.Backend.Application.DTOs.UserDTOs.Requests;
+using SmartEvent.Backend.Application.Interfaces.IServices;
+using SmartEvent.Backend.Application.Services;
+using SmartEvent.Backend.Core.Enums;
+using SmartEvent.Backend.Core.Interfaces.IRepositories;
+using SmartEvent.Backend.Core.Models;
+
+namespace SmartEvent.Backend.Application.Tests;
+
+public class UserServiceTests
+{
+    private readonly IUserRepository _userRepositoryMock;
+    private readonly IPasswordHasher _passwordHasherMock;
+    private readonly IJwtService _jwtServiceMock;
+    private readonly IMapper _mapperMock;
+
+    private readonly UserService _sut;
+
+    public UserServiceTests()
+    {
+        _userRepositoryMock = Substitute.For<IUserRepository>();
+        _passwordHasherMock = Substitute.For<IPasswordHasher>();
+        _jwtServiceMock = Substitute.For<IJwtService>();
+        _mapperMock = Substitute.For<IMapper>();
+
+        _sut = new UserService(_userRepositoryMock, _mapperMock, _passwordHasherMock, _jwtServiceMock);
+    }
+
+    public class Authorize : UserServiceTests
+    {
+        [Fact]
+        public async Task Authorize_ShouldReturnSuccess_WhenDtoValid()
+        {
+            var request = new LoginUserRequestDto()
+            {
+                Email = "test@gmail.com",
+                Password = "password"
+            };
+            
+            string fakeJwtToken = "fakeJwtToken";
+            var fakeUser = new User()
+            {
+                Id = Guid.NewGuid(),
+                Email = request.Email,
+                UserRole = UserRole.Employee,
+                PasswordHash = "fakePasswordHash"
+            };
+            
+            _userRepositoryMock.GetUserByEmail(request.Email).Returns(fakeUser);
+            _passwordHasherMock.Verify(fakeUser.PasswordHash, request.Password).Returns(true);
+            _jwtServiceMock.GenerateJwtToken(fakeUser).Returns(fakeJwtToken);
+
+            var result = await _sut.AuthorizeUserAsync(request);
+            result.IsSuccess.Should().BeTrue();
+            result.Data.Should().NotBeNull();
+            result.Data.JwtToken.Should().Be(fakeJwtToken);
+        }
+    }
+}
