5d7c9936bee92eec2cdb328251ec60e27e57dfb1
mirage-only <137653637+mirage-only@users.noreply.github.com>
2026-08-03 11:57:36 +0300

    feat: add auth logger #80

---
+++ SmartEvent.Backend/SmartEvent.Backend.Infrastructure/ExternalServices/AuthLoggerAnalyticsService/AuthLogger.cs
@@ -0,0 +1,25 @@
+using MassTransit;
+using SmartEvent.Backend.Application.Interfaces.ICommon;
+using SmartEvent.Backend.Application.Interfaces.IServices;
+using SmartEvent.Backend.Shared.Contracts.Analytics;
+using SmartEvent.Backend.Shared.Contracts.Analytics.Enums;
+
+namespace SmartEvent.Backend.Infrastructure.ExternalServices.AuthLoggerAnalyticsService;
+
+public class AuthLogger(IUserContext userContext, IPublishEndpoint publishEndpoint): IAuthLogger
+{
+    public async Task LogAuthAsync(string email, Guid? userId, AuthStatusEnum status, string? failureReason = null)
+    {
+        var logEvent = new AuthLogEvent
+        {
+            UserEmail = email,
+            UserId = userId,
+            Status = status,
+            FailureReason = failureReason,
+            IpAddress = userContext.IpAddress,
+            UserAgent = userContext.UserAgent
+        };
+        
+        await publishEndpoint.Publish(logEvent);
+    }
+}
